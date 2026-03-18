885d1bc4341e8d8e5b66d47ba282d0ef06a5b7d7
LuSiama <tata25200412@gmail.com>
2026-03-18 15:14:38 +0300

    Попытка включения tytest

--- test/test_fix.py
+++ test/test_fix.py
@@ -6,11 +6,3 @@ def file_one():
 
 def test_file_one(file_one):
     assert file_one == 22
-
-def my_function(a, b):
-    return a + b
-
-print(my_function(1, 2))
-
-def test_my_function():
-    assert my_function(1, 2) == 3
--- test_example.py
+++ test_example.py
@@ -7,7 +7,7 @@ print(my_function(1, 2))
 def test_my_function():
     assert my_function(1, 2) == 3
 
-
+# тест с ошибкой
 # def suite():
 #     suite = unittest.TestSuite()
 #     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test_my_function))
