c12187750238e9eae3deadc3c028dc70335a0276
LuSiama <tata25200412@gmail.com>
2026-03-19 13:58:17 +0000

    Редактировать test_example.py

--- test_example.py
+++ test_example.py
@@ -7,13 +7,12 @@ print(my_function(1, 2))
 def test_my_function():
     assert my_function(1, 2) == 3
 
-# тест с ошибкой
 # def suite():
 #     suite = unittest.TestSuite()
 #     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test_my_function))
 #     return suite
 
 
-
-# def test_my_function_error():
-#     assert my_function(1, 2) == 5
+# тест с ошибкой
+def test_my_function_error():
+    assert my_function(1, 2) == 5
