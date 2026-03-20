893f066de19865041c212fa9b5f792540d8a6592
tatyana_d <tatyana_d@medicine-it.ru>
2026-03-20 16:45:18 +0300

    *

--- test_example.py
+++ test_example.py
@@ -1,17 +1,10 @@
-import pytest
 def my_function(a, b):
     return a + b
 
 print(my_function(1, 2))
 
-def test_my_function():
-    assert my_function(1, 2) == 3
-
-# def suite():
-#     suite = unittest.TestSuite()
-#     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test_my_function))
-#     return suite
-
+# def test_my_function():
+#     assert my_function(1, 2) == 3
 
 # тест с ошибкой
 def test_my_function_error():
