8391cb820902acef44acca8cfe3de1e6a9cf2dd0
CI <ci@example.com>
2026-03-23 17:22:18 +0300

    *

--- test/test_fix.py
+++ test/test_fix.py
@@ -1,8 +1,10 @@
 import pytest
 
+
 @pytest.fixture()
 def file_one():
     return 22
+
 
 def test_file_one(file_one):
     assert file_one == 22
--- test_example.py
+++ test_example.py
@@ -1,18 +1,19 @@
-import pytest
 def my_function(a, b):
     return a + b
 
+
 print(my_function(1, 2))
+
 
 def test_my_function():
     assert my_function(1, 2) == 3
+
 
 # тест с ошибкой
 # def suite():
 #     suite = unittest.TestSuite()
 #     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test_my_function))
 #     return suite
-
 
 
 # def test_my_function_error():
