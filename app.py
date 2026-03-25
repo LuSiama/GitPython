0da9d5c690f85de3adfde5a29871a045aeb5baf3
tatyana_d <tatyana_d@medicine-it.ru>
2026-03-25 11:40:54 +0300

    Изменения в app.py 1 (проверка pyll)

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask, request
 import git
 app = Flask(__name__)
 
-@app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('https://tatiyana.pythonanywhere.com/')
-            origin = repo.remotes.origin
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://tatiyana.pythonanywhere.com')
+        origin = repo.remotes.origin
 
-            origin.pull()
-            return '', 200
-        else:
-            return 'Что-то пошло не так', 400
+        origin.pull()
+        return '', 200
+    else:
+        return 'Что-то пошло не так', 400
 
 # @app.route("/")
 # def home():
 #     return "Вовка морковка!"
-
-if __name__ == '__main__':
-    app.run(host='192.168.136.138')
+#
+# if __name__ == '__main__':
+#     app.run(host='192.168.136.138')
