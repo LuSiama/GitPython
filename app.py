819faff88c3c0d35fbf480996eea006c0b72b2fd
tatyana_d <tatyana_d@medicine-it.ru>
2026-03-25 11:29:23 +0300

    ***

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request
+import git
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Вовка морковка!"
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('https://tatiyana.pythonanywhere.com/')
+            origin = repo.remotes.origin
+
+            origin.pull()
+            return '', 200
+        else:
+            return 'Что-то пошло не так', 400
+
+# @app.route("/")
+# def home():
+#     return "Вовка морковка!"
 
 if __name__ == '__main__':
     app.run(host='192.168.136.138')
