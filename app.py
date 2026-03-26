fa75a1ec8c3562637e2c85791d67afc32e007ec7
CI <ci@example.com>
2026-03-26 09:47:05 +0300

    Изменение надпсии в app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Вовка морковка1!"
+    return "Вовка морковка!"
 
 if __name__ == '__main__':
     app.run(host='192.168.136.138')
